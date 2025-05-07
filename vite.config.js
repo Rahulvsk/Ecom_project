41a9d2eafed3143f1c84c8e4c32c803d8b338bbc
Rahulvsk <92639667+Rahulvsk@users.noreply.github.com>
2025-05-07 22:46:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { webcrypto as crypto } from 'crypto'
+globalThis.crypto = crypto
 
 // https://vite.dev/config/
 export default defineConfig({
